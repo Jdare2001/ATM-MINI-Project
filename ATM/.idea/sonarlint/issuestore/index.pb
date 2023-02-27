2807f7d873e0b1dc6d8e593642758275db4ec496
Jayden <59065556+Jdare2001@users.noreply.github.com>
2023-02-27 22:48:15 +0000

    Starting commit
    initialising the start of the project

---
+++ ATM/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+E
+src/Classes/Main.java,6\6\66918574b79527ef526f6a163a4c663512f27d7c
+`
+0src/Exceptions/InsufficientBalanceException.java,f\e\fe328ada9e57fc18427e0133dde084b6bcc99304
+^
+.src/Exceptions/IncorrectPasswordException.java,f\3\f38dd02382e31118d65819668a0cbbcebabc973a
+E
+src/Classes/Bank.java,4\d\4d3ab4c30d432d008bfce0d10dc542afdbd11acb
+H
+src/Classes/Account.java,8\0\8095762f0644fb447f532c59feb4ec43aa2bd121
---
+++ ATM/src/Classes/Account.java
@@ -0,0 +1,57 @@
+package Classes;
+
+import Exceptions.IncorrectPasswordException;
+import Exceptions.InsufficientBalanceException;
+
+public class Account {
+    private String id;
+    private String password;
+    private int balance;
+    private int overdraftAllowed = 0;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public int getBalance(String p) throws IncorrectPasswordException {
+        if(passwordCheck(p)) {
+            return balance;
+        }
+        throw new IncorrectPasswordException();
+    }
+
+    public int OverDrawnAllowedAmount(String p) throws IncorrectPasswordException {
+        if(passwordCheck(p)) {
+            return overdraftAllowed;
+        }
+        throw new IncorrectPasswordException();
+    }
+    public int depositMoney(String p, int depositAmount){
+        if(passwordCheck(p)){
+            balance = balance + depositAmount;
+            return balance;
+        }
+        throw new IllegalStateException("incorrect password");
+    }
+    public int withdrawMoney(String p, int withdrawAmount) throws IncorrectPasswordException, InsufficientBalanceException {
+        if(passwordCheck(p)){
+            if((balance  + overdraftAllowed) - withdrawAmount > 0 ){
+                balance = balance - withdrawAmount;
+                return balance;
+            }
+            else{
+                throw new InsufficientBalanceException();
+            }
+        }
+       throw new IncorrectPasswordException();
+    }
+    private boolean passwordCheck(String p){
+        boolean returnedStatement = false;
+        if(p.equals(password)){
+            returnedStatement = true;
+        }
+        return returnedStatement;
+
+    }
+}
