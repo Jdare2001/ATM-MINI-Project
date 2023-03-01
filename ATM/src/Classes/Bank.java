92a487a9afb410a7e75d5bf1471a4c8c0cb08030
Jayden <59065556+Jdare2001@users.noreply.github.com>
2023-03-01 15:49:44 +0000

    started on bank class
    added the beginnings of the bank class and created the Array list

--- ATM/src/Classes/Bank.java
+++ ATM/src/Classes/Bank.java
@@ -1,4 +1,18 @@
 package Classes;
 
+import java.util.ArrayList;
+
 public class Bank {
+
+    private ArrayList accountList;
+
+    public Bank() {
+        accountList = new ArrayList<Account>();
+    }
+
+    private void addAccount(Account a){
+        if(!accountList.contains(a)){
+            accountList.add(a);
+        }
+    }
 }
